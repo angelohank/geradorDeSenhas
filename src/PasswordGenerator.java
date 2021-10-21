85a1828ff387f162c225eb764f11a214292bff52
Ângelo Hank <angelohank@alunos.utfpr.edu.br>
2021-10-21 15:42:10 -0300

    adicionando numeros

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -3,18 +3,19 @@ public final class PasswordGenerator {
     private int passwordLength;
     private char[] caracteres = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '!', '@', '#', '$', '%', '&', '*', '(', ')', '{', '}', '[', ']', '?', '/'
+            '!', '@', '#', '$', '%', '&', '*', '(', ')', '{', '}', '[', ']', '?', '/',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
 
     String passwordGenerate = "";
 
     public PasswordGenerator(int passwordNivel) {
         if (passwordNivel == 1) {
-            this.passwordLength = 6;
-        } else if (passwordNivel == 2) {
-            this.passwordLength = 8;
-        } else {
             this.passwordLength = 10;
+        } else if (passwordNivel == 2) {
+            this.passwordLength = 12;
+        } else {
+            this.passwordLength = 16;
         }
     }
 
